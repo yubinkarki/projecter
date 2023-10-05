082cecc3bead7ea26d5ef02c37b1189935584aa5
yubinkarki <iamyubinkarki@gmail.com>
2023-10-05 12:22:24 +0545

    Rename to ts extension

---
+++ projecter_backend/src/database/Connection.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+import {
+  db_connection_success,
+  db_connection_fail,
+} from "@/constants/AppStrings";
+
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(process.env.DB || "No Url")
+  .then(() => {
+    console.log(db_connection_success);
+  })
+  .catch((err) => {
+    console.log(db_connection_fail, err);
+  });
+
+module.exports;
