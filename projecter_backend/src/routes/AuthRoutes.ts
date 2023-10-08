2c959ed74fa44d14bf00f6ace87d53df8ee415c9
yubinkarki <iamyubinkarki@gmail.com>
2023-10-08 12:35:14 +0545

    Remove unused express

--- projecter_backend/src/routes/AuthRoutes.ts
+++ projecter_backend/src/routes/AuthRoutes.ts
@@ -1,8 +1,8 @@
-import express, { Router } from "express";
+import { Router } from "express";
 
 import { signup, login } from "@/controllers/AuthController";
 
-const router: Router = express.Router();
+const router: Router = Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
