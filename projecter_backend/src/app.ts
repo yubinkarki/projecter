8ba7b01501527038109b8031aaa456f670260f8f
yubinkarki <iamyubinkarki@gmail.com>
2023-10-08 12:34:16 +0545

    Imports refactor

--- projecter_backend/src/app.ts
+++ projecter_backend/src/app.ts
@@ -1,35 +1,28 @@
 import cors from "cors";
-import dotenv from "dotenv";
 import express, { Express } from "express";
 
+import config from "@/config/EnvConfig";
 import authRoutes from "@/routes/AuthRoutes";
-
-// Loads the contents of .env file into process.env to use it.
-dotenv.config();
+import { connectToDb } from "@/database/Connection";
+import { server_start_success, server_start_failure } from "@/constants/AppStrings";
 
 const app: Express = express();
-const port: String = process.env.PORT || "8545";
 
 // Built in middleware function in express. https://expressjs.com/en/api.html
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Connection to database.
-require("./database/Connection");
+connectToDb();
 
 app.use(cors());
 
-// Routes.
 app.use("/user", require("./routes/UserRoutes"));
 app.use("/task", require("./routes/TaskRoutes"));
 app.use("/auth", authRoutes);
 app.use("/project", require("./routes/ProjectRoutes"));
 
-// Start development server.
 try {
-  app.listen(port, () => {
-    console.log(`Server successfully started at port ${port}`);
-  });
+  app.listen(config.port, () => console.log(`${server_start_success} at port ${config.port}`));
 } catch (err) {
-  console.log("Could not start the server", err);
+  console.log(`${server_start_failure}`, err);
 }
