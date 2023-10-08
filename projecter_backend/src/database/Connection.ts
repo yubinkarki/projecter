13ce5666a3fda5e1f01f331a95cd136fddc9ef88
yubinkarki <iamyubinkarki@gmail.com>
2023-10-08 12:35:00 +0545

    Use string constant

--- projecter_backend/src/database/Connection.ts
+++ projecter_backend/src/database/Connection.ts
@@ -1,19 +1,16 @@
 import mongoose from "mongoose";
 
-import {
-  db_connection_success,
-  db_connection_fail,
-} from "@/constants/AppStrings";
+import { db_connection_success, db_connection_fail, cannot_find_db_url } from "@/constants/AppStrings";
 
 mongoose.set("strictQuery", false);
 
-mongoose
-  .connect(process.env.DB || "No Url")
-  .then(() => {
-    console.log(db_connection_success);
-  })
-  .catch((err: Error) => {
-    console.log(db_connection_fail, err);
-  });
-
-module.exports;
+export const connectToDb = (): void => {
+  mongoose
+    .connect(process.env.DB || cannot_find_db_url)
+    .then(() => {
+      console.log(db_connection_success);
+    })
+    .catch((err: Error) => {
+      console.log(db_connection_fail, err);
+    });
+};
