7f51d688486a1ab75075acb6c0e44dabc1cf7e9f
yubinkarki <iamyubinkarki@gmail.com>
2023-11-30 17:31:22 +0545

    Move some routes from user to auth

--- projecter_backend/src/routes/AuthRoutes.ts
+++ projecter_backend/src/routes/AuthRoutes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 
-import { signup, login } from "@/controllers/AuthController";
+import { authentication } from "@/utils";
+import { signup, login, deleteOneUser, updateUser, updatePassword } from "@/controllers/AuthController";
 
 const router: Router = Router();
 
 router.post("/login", login);
 router.post("/signup", signup);
+
+router.delete("/delete/:id", deleteOneUser);
+
+router.put("/update", [authentication], updateUser);
+router.put("/password/update", [authentication], updatePassword);
 
 export default router;
--- projecter_backend/src/routes/UserRoutes.ts
+++ projecter_backend/src/routes/UserRoutes.ts
@@ -1,16 +1,6 @@
 import { Router } from "express";
 
-import {
-  getAllUsers,
-  signup,
-  login,
-  getOneUser,
-  deleteOneUser,
-  updateUser,
-  updatePassword,
-  getOneUserById,
-  getManyUsers,
-} from "@/controllers/UserController";
+import { getAllUsers, getOneUser, getOneUserById, getManyUsers } from "@/controllers/UserController";
 import { authentication } from "@/utils";
 
 const router: Router = Router();
@@ -19,13 +9,5 @@ router.get("/getall", getAllUsers);
 router.get("/getmany", getManyUsers);
 router.get("/getoneuser/:id", getOneUserById);
 router.get("/getone", [authentication], getOneUser);
-
-router.post("/login", login);
-router.post("/signup", signup);
-
-router.put("/update", [authentication], updateUser);
-router.put("/password/update", [authentication], updatePassword);
-
-router.delete("/delete/:id", deleteOneUser);
 
 export default router;
