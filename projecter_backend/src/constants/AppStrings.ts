3e3820c9d7d409a354dbd6714bd8659d6d2de940
yubinkarki <iamyubinkarki@gmail.com>
2023-12-15 17:56:28 +0545

    Add string constant

--- projecter_backend/src/constants/AppStrings.ts
+++ projecter_backend/src/constants/AppStrings.ts
@@ -2,6 +2,7 @@ export const appStrings: { [key: string]: string } = {
   stagEnv: "staging",
   devEnv: "development",
   fallback_port: "8545",
+  secret_jwt_key: "Secret",
   cannot_find_env: "Cannot find .env file",
   cannot_find_db_url: "Cannot find DB Url",
   server_start_failure: "Failed to start server",
