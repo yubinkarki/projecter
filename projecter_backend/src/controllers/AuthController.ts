a840720db55313930a8dd7324c34f63302fcea30
yubinkarki <iamyubinkarki@gmail.com>
2023-12-17 13:16:51 +0545

    Fix signup error handling

--- projecter_backend/src/controllers/AuthController.ts
+++ projecter_backend/src/controllers/AuthController.ts
@@ -1,64 +1,72 @@
-import { Response, Request } from "express";
+import {pick} from "lodash";
+import {compare} from "bcrypt";
+import {Response, Request} from "express";
 
-import { userModel } from "@/models"; // User database model.
-import { sendToken } from "@/utils"; // Create JWT token function.
+import {sendToken} from "@/utils"; // Create JWT token.
+import {userModel} from "@/models"; // User database model.
+import {Designation} from "@/constants";
 
-const { pick } = require("lodash");
-const bcrypt = require("bcrypt");
+interface NewUserFormData {
+  email: string;
+  lastName: Date;
+  firstName: Date;
+  password: string;
+  phoneNumber: string;
+  designation: Designation;
+}
+
+const pickReqUserData: (keyof NewUserFormData)[] = [
+  "email",
+  "password",
+  "lastName",
+  "firstName",
+  "phoneNumber",
+  "designation",
+];
 
 // Create user.
 export const signup = async (req: Request, res: Response) => {
   try {
-    let userData = pick(req.body, ["firstName", "lastName", "email", "password", "phoneNumber", "designation"]);
+    const userData: NewUserFormData = pick(req.body, pickReqUserData);
 
-    await userModel.create(userData);
+    const result = await userModel.create(userData);
 
-    return res.status(201).json({ status: true, msg: "User created successfully" });
+    if (!result) res.status(500).json({status: false, msg: "Failed to create user"});
+
+    return res.status(201).json({status: true, msg: "User created successfully"});
   } catch (err: any) {
-    if (err.keyPattern.email === 1) {
-      res.status(400).json({ status: false, msg: "Email already exists" });
-
-      return null;
-    } else if (err.keyPattern.phoneNumber === 1) {
-      res.status(400).json({ status: false, msg: "Phone number already exists" });
-
-      return null;
-    } else {
-      res.status(400).json({ status: false, msg: "Error in creating user" });
-
-      return null;
-    }
+    return res.status(500).json({status: false, msg: err.errors});
   }
 };
 
 // Login user.
 export const login = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-  const login = await userModel.findOne({ email }).select("+password");
+  const {email, password} = req.body;
+  const login = await userModel.findOne({email}).select("+password");
 
   try {
-    if (!login) res.status(404).json({ emailError: true, msg: "Could not find email" });
+    if (!login) res.status(404).json({emailError: true, msg: "Could not find email"});
 
-    const verify = await bcrypt.compare(password, login?.password);
+    const verify = await compare(password, login?.password ?? "");
 
-    if (!verify) res.status(401).json({ passwordError: true, msg: "Incorrect password" });
+    if (!verify) res.status(401).json({passwordError: true, msg: "Incorrect password"});
 
     return sendToken(login, 202, res);
-  } catch (err) {
-    return res.status(401).json({ status: false, msg: "Could not login" });
+  } catch (err: any) {
+    res.status(500).json({status: false, msg: err.errors});
   }
 };
 
 // Delete user by id.
 export const deleteOneUser = async (req: Request, res: Response) => {
   try {
-    const user = await userModel.findByIdAndRemove(req.params.id);
+    const user = await userModel.findByIdAndDelete(req.params.id);
 
-    if (!user) return res.status(404).json({ noUser: true, msg: "User not found" });
+    if (!user) return res.status(404).json({noUser: true, msg: "User not found"});
 
-    return res.status(202).json({ status: true, msg: "User deleted successfully" });
+    return res.status(202).json({status: true, msg: "User deleted successfully"});
   } catch (err) {
-    return res.status(400).json({ status: false, msg: "Error deleting user" });
+    return res.status(400).json({status: false, msg: "Error deleting user"});
   }
 };
 
@@ -69,11 +77,11 @@ export const updateUser = async (req: Request, res: Response) => {
       new: true,
     });
 
-    if (!user) res.status(404).json({ noUser: true, msg: "User not found" });
+    if (!user) res.status(404).json({noUser: true, msg: "User not found"});
 
-    return res.status(202).json({ status: true, msg: "User updated successfully" });
+    return res.status(202).json({status: true, msg: "User updated successfully"});
   } catch (err) {
-    return res.status(404).json({ status: false, msg: "Error updating users", err });
+    return res.status(404).json({status: false, msg: "Error updating users", err});
   }
 };
 
@@ -84,7 +92,7 @@ export const updatePassword = async (req: Request, res: Response) => {
     const passwordMatch = await user?.comparePassword(req.body.currentPassword);
 
     if (!passwordMatch) {
-      return res.status(400).json({ passwordError: true, msg: "Incorrect current password" });
+      return res.status(400).json({passwordError: true, msg: "Incorrect current password"});
     }
 
     if (req.body.newPassword !== req.body.confirmPassword) {
@@ -99,8 +107,8 @@ export const updatePassword = async (req: Request, res: Response) => {
       await user?.save();
     }
 
-    return res.status(200).json({ status: true, msg: "Password updated successfully" });
+    return res.status(200).json({status: true, msg: "Password updated successfully"});
   } catch (err) {
-    return res.status(400).json({ status: false, msg: "Could not update password", err });
+    return res.status(400).json({status: false, msg: "Could not update password", err});
   }
 };
