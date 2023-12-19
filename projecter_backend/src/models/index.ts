de16200b787e7f4ade78468a3d1db7e7e1118196
yubinkarki <iamyubinkarki@gmail.com>
2023-12-19 11:07:43 +0545

    Minor formatting change

--- projecter_backend/src/models/index.ts
+++ projecter_backend/src/models/index.ts
@@ -1,5 +1,5 @@
 import taskModel from "./TaskModel";
 import projectModel from "./ProjectModel";
-import userModel, { userSchema } from "./UserModel";
+import userModel, {userSchema} from "./UserModel";
 
-export { userModel, userSchema, projectModel, taskModel };
+export {userModel, userSchema, projectModel, taskModel};
