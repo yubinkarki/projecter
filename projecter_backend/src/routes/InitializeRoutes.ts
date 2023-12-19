5aed03cc069f4c052e2f99a7bbe4305551fdb804
yubinkarki <iamyubinkarki@gmail.com>
2023-12-19 11:08:19 +0545

    Manage home route

--- projecter_backend/src/routes/InitializeRoutes.ts
+++ projecter_backend/src/routes/InitializeRoutes.ts
@@ -1,15 +1,25 @@
-import {Express} from "express";
+import {Express, Response, Request} from "express";
 
-import {undefinedRoute, homeRoute} from "@/utils";
+import {appStrings} from "@/constants";
+import {undefinedRoute} from "@/utils";
 import {authRoutes, projectRoutes, taskRoutes, userRoutes} from "@/routes";
 
+const {serverRunning} = appStrings;
+
 const initializeRoutes = (app: Express): void => {
-  app.use("/", homeRoute);
-  app.use("*", undefinedRoute);
   app.use("/user", userRoutes);
   app.use("/task", taskRoutes);
   app.use("/auth", authRoutes);
   app.use("/project", projectRoutes);
+
+  app.get("/", (_: Request, res: Response) => {
+    res.json({
+      status: true,
+      message: serverRunning,
+    });
+  });
+
+  app.use("*", undefinedRoute);
 };
 
 export default initializeRoutes;
