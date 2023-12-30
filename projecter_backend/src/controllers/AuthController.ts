72fa86f8c8e0a2f5742c2d0beb7b29c7174c3ee7
yubinkarki <iamyubinkarki@gmail.com>
2023-12-30 10:31:36 +0545

    Fix update user and password controllers

--- projecter_backend/src/controllers/AuthController.ts
+++ projecter_backend/src/controllers/AuthController.ts
@@ -6,6 +6,14 @@ import {sendToken} from "@/utils"; // Create JWT token.
 import {userModel} from "@/models"; // User database model.
 import {NewUserFormData} from "@/constants";
 
+interface AuthenticatedRequest extends Request {
+  user?: {
+    id: string;
+    iat: number;
+    exp: number;
+  };
+}
+
 const pickReqUserData: (keyof NewUserFormData)[] = [
   "email",
   "password",
@@ -62,44 +70,50 @@ export const deleteOneUser = async (req: Request, res: Response) => {
 };
 
 // Update user details.
-export const updateUser = async (req: Request, res: Response) => {
-  try {
-    const user = await userModel.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
+export const updateUser = async (req: AuthenticatedRequest, res: Response) => {
+  if (req.user) {
+    try {
+      const {id} = req.user;
 
-    if (!user) res.status(404).json({noUser: true, msg: "User not found"});
+      await userModel.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
 
-    return res.status(202).json({status: true, msg: "User updated successfully"});
-  } catch (err) {
-    return res.status(404).json({status: false, msg: "Error updating users", err});
+      return res.status(202).json({status: true, msg: "User updated successfully"});
+    } catch (err) {
+      return res.status(404).json({status: false, msg: "Error updating users", err});
+    }
   }
+
+  return res.status(404).json({noUser: true, msg: "User not found"});
 };
 
 // Update user password.
-export const updatePassword = async (req: Request, res: Response) => {
-  try {
-    const user = await userModel.findById(req.params.id).select("+password");
-    const passwordMatch = await user?.comparePassword(req.body.currentPassword);
+export const updatePassword = async (req: AuthenticatedRequest, res: Response) => {
+  if (req.user) {
+    try {
+      const {id} = req.user;
+      const user = await userModel.findById(id).select("+password");
+      const passwordMatch = await user?.comparePassword(req.body.currentPassword);
 
-    if (!passwordMatch) {
-      return res.status(400).json({passwordError: true, msg: "Incorrect current password"});
+      if (!passwordMatch) res.status(400).json({passwordError: true, msg: "Incorrect current password"});
+
+      if (req.body.newPassword !== req.body.confirmPassword)
+        res.status(400).json({
+          confirmPasswordError: true,
+          msg: "Password does not match with each other",
+        });
+
+      if (user != null) {
+        user.password = req.body.newPassword;
+        await user?.save();
+      }
+
+      return res.status(200).json({status: true, msg: "Password updated successfully"});
+    } catch (err) {
+      return res.status(400).json({status: false, msg: "Could not update password", err});
     }
-
-    if (req.body.newPassword !== req.body.confirmPassword) {
-      return res.status(400).json({
-        confirmPasswordError: true,
-        msg: "Password does not match with each other",
-      });
-    }
-
-    if (user != null) {
-      user.password = req.body.newPassword;
-      await user?.save();
-    }
-
-    return res.status(200).json({status: true, msg: "Password updated successfully"});
-  } catch (err) {
-    return res.status(400).json({status: false, msg: "Could not update password", err});
   }
+
+  return res.status(404).json({noUser: true, msg: "User not found"});
 };
